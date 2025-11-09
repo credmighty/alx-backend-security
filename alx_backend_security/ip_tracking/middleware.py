07ae7af4385dda24341b5c08ccd18177a9bc7b39
Abdullah Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2025-11-09 10:05:58 +0100

    security: filtering geolocation and ip addressed

---
+++ alx_backend_security/ip_tracking/middleware.py
@@ -0,0 +1,50 @@
+from django.http import HttpResponseForbidden
+from .models import RequestLog, BlockedIP
+from datetime import datetime
+from ip_geolocation.ip import IPGeolocationAPI
+from django.core.cache import cache
+
+class IPTrackingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.geo = IPGeolocationAPI() # Initialize the geolocation API
+
+    def __call__(self, request):
+        #Get IP address
+        ip_address = self.get_client_ip(request)
+        path = request.path
+
+        # check if IP is blocked
+        if BlockedIP.objects.filter(ip_address=ip_address).exists():
+            return HttpResponseForbidden('Access Denied: Your IP has been blocked.')
+        
+
+        # Get geolocation (cached for 24hrs)
+        location_data = cache.get(ip_address)
+        if not location_data:
+            data = self.geo.get_geolocation(ip_address)
+            country = data.get('country_name', 'Unknown')
+            city = data.get('city', 'Unknown')
+            location_data = {'country': country, 'city': city}
+            cache.set(ip_address, location_data, timeout=60 * 60 * 24) #24hrs in seconds
+
+
+        #save request info to database
+        RequestLog.objects.create(
+            ip_address=ip_address,
+            path=path,
+            country=location_data["country"],
+            city=location_data["city"],
+        )
+
+        response = self.get_response(request)
+        return response
+
+    def get_client_ip(self, request):
+        """Get the user's real IP even if behind a proxy"""
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
---
+++ alx_backend_security/ip_tracking/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class RequestLog(models.Model):
+    ip_address = models.GenericIPAddressField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    path = models.CharField(max_length=255)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.path} at {self.timestamp} ({self.country}, {self.city})"
+
+
+class BlockedIP(models.Model):
+    ip_address = models.GenericIPAddressField(unique=True)
+
+    def __str__(self):
+        return f"{self.ip_address}"
